dff8b46d4efe8e1aa4e200082052d716b41b9187
アクツユミ <akutsu@m-g-n.me>
2022-02-17 15:31:23 +0900

    全パターンの作成・調整など

--- block-styles/snow-monkey-blocks/items/RJE_R001CORP_banners/register.php
+++ block-styles/snow-monkey-blocks/items/RJE_R001CORP_banners/register.php
@@ -10,9 +10,10 @@ $override_block_name = 'snow-monkey-blocks/items';
 $block_style_label   = '類人猿R001CORP バナー';
 
 //各ファイルパスなどを設定.
-$basename = basename( __DIR__ );
-$front_filename  = 'dist/css/block-styles/' . $override_block_name . '/' . $basename . '/style-front.css';
-$editor_filename = 'dist/css/block-styles/' . $override_block_name . '/' . $basename . '/style-editor.css';
+$basename      = basename( __DIR__ );
+$dist_dir_path = 'dist/css/block-styles/' . $override_block_name . '/' . $basename . '/';
+$front_filename  = $dist_dir_path . 'style-front.css';
+$editor_filename = $dist_dir_path . 'style-editor.css';
 
 $front_filetime = ( file_exists( RJE_R001CORP_PATH . $front_filename ) ) ? filemtime( RJE_R001CORP_PATH . $front_filename ) : NULL;
 $editor_filetime = ( file_exists( RJE_R001CORP_PATH . $editor_filename ) ) ? filemtime( RJE_R001CORP_PATH . $editor_filename ) : NULL;
@@ -26,6 +27,9 @@ register_block_style(
 	)
 );
 
-//フロント・エディター用のCSSファイルを登録
+//フロント用のCSSファイルを登録
 wp_register_style( 'is-style-' . $basename . '-front', RJE_R001CORP_URL . $front_filename, $this->style_front_deps, $front_filetime );
-wp_register_style( 'is-style-' . $basename . '-editor', RJE_R001CORP_URL . $editor_filename, $this->style_editor_deps, $editor_filetime );
+
+//エディター用のCSSファイルを登録
+add_editor_style('/../../plugins/'.RJE_R001CORP_DIRNAME.'/'.$front_filename);
+// wp_register_style( 'is-style-' . $basename . '-editor', RJE_R001CORP_URL . $editor_filename, $this->style_editor_deps, $editor_filetime ); //TODO：必要なければ消す
