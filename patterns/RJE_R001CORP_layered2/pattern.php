8553035bfc96bf36154b67e6f65dc20efa3e1cef
アクツユミ <akutsu@m-g-n.me>
2022-03-16 16:42:32 +0900

    不要なmetaタグの削除

--- patterns/RJE_R001CORP_layered2/pattern.php
+++ patterns/RJE_R001CORP_layered2/pattern.php
@@ -11,7 +11,7 @@
 
 <!-- wp:snow-monkey-blocks/section-side-heading {"headingColumnSize":"33","containerAlign":"full","fixedBackgroundColor":"#ffffff"} -->
 <div class="wp-block-snow-monkey-blocks-section-side-heading smb-section smb-section-side-heading smb-section--fit"><div class="smb-section__fixed-background" style="padding-top:0;padding-bottom:0;background-color:#ffffff"></div><div class="smb-section__inner"><div class="c-container"><div class="smb-section__contents-wrapper"><div class="c-row c-row--md-margin"><div class="c-row__col c-row__col--1-1 c-row__col--md-1-3"><div class="smb-section__header smb-section-side-heading__header"><h2 class="smb-section__title smb-section-side-heading__title">誰も見たことの<br>ない進化。</h2></div></div><div class="c-row__col c-row__col--1-1 c-row__col--md-2-3"><div class="smb-section__body smb-section-side-heading__body"><!-- wp:paragraph -->
-<p><meta charset="utf-8">猿人は原人へ、そして原人は旧人へ。その中から広がったホモ・サピエンスが、長い時間を経て、今を生きる私たちへとつながっていきました。<br>私たちは人間です。そして今、進化を遂げようとしている、<br>Snow Monkeyと共に歩みます。</p>
+<p>猿人は原人へ、そして原人は旧人へ。その中から広がったホモ・サピエンスが、長い時間を経て、今を生きる私たちへとつながっていきました。<br>私たちは人間です。そして今、進化を遂げようとしている、<br>Snow Monkeyと共に歩みます。</p>
 <!-- /wp:paragraph --></div></div></div></div></div></div></div>
 <!-- /wp:snow-monkey-blocks/section-side-heading --></div></div></div></div></div>
 <!-- /wp:snow-monkey-blocks/section -->
